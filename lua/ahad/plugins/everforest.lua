e98db1380ad33d5698e21761454513c6998fae0a
Muhammed Ahad <maaahad@gmail.com>
2024-06-26 16:56:02 +0200

    sainnhe everforest theme ref

--- lua/ahad/plugins/everforest.lua
+++ lua/ahad/plugins/everforest.lua
@@ -1,3 +1,17 @@
+-- -- sainnhe
+-- return {
+--   "sainnhe/everforest",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     -- Optionally configure and load the colorscheme
+--     -- directly inside the plugin declaration.
+--     vim.g.everforest_enable_italic = true
+--     vim.cmd.colorscheme("everforest")
+--   end,
+-- }
+
+-- neanias
 return {
   "neanias/everforest-nvim",
   version = false,

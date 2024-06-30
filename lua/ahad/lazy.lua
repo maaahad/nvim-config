6591ff18ea91bae7b99b528691302ccc224a7f23
Muhammed Ahad <maaahad@gmail.com>
2024-06-30 21:20:17 +0200

    enabled yanky

--- lua/ahad/lazy.lua
+++ lua/ahad/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
